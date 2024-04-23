ce7cdcd48e64f7f95f9b432a6cfad7900aa09f78
ingridTPires <ingrid.teixeira@trindadetecnologia.com.br>
2024-04-23 17:47:18 -0300

    Função HelloWorld

---
+++ src/HelloWorld/HelloWorld/Function.cs
@@ -0,0 +1,13 @@
+﻿using Google.Cloud.Functions.Framework;
+using Microsoft.AspNetCore.Http;
+
+namespace HelloWorld
+{
+    public class Function : IHttpFunction
+    {
+        public async Task HandleAsync(HttpContext context)
+        {
+            await context.Response.WriteAsync("Hello World!");
+        }
+    }
+}
